1f3db2c113dcf66404c2366cee988b6b3804a03f
GabrielAbreuSTD <gabrielsilvaabreuif@gmail.com>
2019-11-04 11:18:28 -0200

    primeira_versao

---
+++ src/app/enviar/enviar.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enviar',
+  templateUrl: './enviar.page.html',
+  styleUrls: ['./enviar.page.scss'],
+})
+export class EnviarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
